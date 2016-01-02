badf7630654cf25c07724091f3ac01949a33ef32
chengcheng <1038127753@qq.com>
2016-01-02 19:20:56 +0800

    提交

---
+++ app/src/main/java/com/lcc/base/BaseApplication.java
@@ -0,0 +1,10 @@
+package com.lcc.base;
+
+/**
+ * Author:  梁铖城
+ * Email:   1038127753@qq.com
+ * Date:    2015年12月15日10:47:52
+ * Description:   BaseApplication
+ */
+public class BaseApplication {
+}
