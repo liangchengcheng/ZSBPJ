f325cbe738d2d026c4ec4c241fc127ed5abdb240
chengcheng <1038127753@qq.com>
2016-01-02 22:01:06 +0800

    提交了fragment的基本的示例代码

--- app/src/main/java/com/lcc/activity/MainActivity.java
+++ app/src/main/java/com/lcc/activity/MainActivity.java
@@ -2,7 +2,12 @@ package com.lcc.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +17,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.lcc.activity.main.fragment.AllKnowFragment;
+import com.lcc.activity.main.fragment.HomeFragment;
+import com.lcc.activity.main.fragment.OnlineClassFragment;
 import com.lcc.base.BaseActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -32,6 +42,25 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         mDrawerLayout.setDrawerListener(toggle);
         mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
+        setViewPager();
+    }
+
+    private void setViewPager(){
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "快速导航");
+        adapter.addFragment(new AllKnowFragment(), "专升本百科");
+        adapter.addFragment(new OnlineClassFragment(), "在线课程");
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
     }
 
     @Override
@@ -94,6 +123,36 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
         }
     }
 }
---
+++ app/src/main/java/com/lcc/activity/main/fragment/OnlineClassFragment.java
@@ -0,0 +1,26 @@
+package com.lcc.activity.main.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lcc.activity.R;
+
+/**
+ * Author:  梁铖城
+ * Email:   1038127753@qq.com
+ * Date:    2015年12月15日10:47:52
+ * Description:  在线课程
+ */
+public class OnlineClassFragment  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_online, null);
+        return  view;
+    }
+}
