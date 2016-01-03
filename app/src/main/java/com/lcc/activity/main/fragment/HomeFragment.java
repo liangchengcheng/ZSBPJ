3cc2541ab965d41e1d64394c66c8ca4a2886ef9b
chengcheng <1038127753@qq.com>
2016-01-03 22:28:08 +0800

    增加了首页轮番的实现

--- app/src/main/java/com/lcc/activity/main/fragment/HomeFragment.java
+++ app/src/main/java/com/lcc/activity/main/fragment/HomeFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.lcc.activity.R;
+import com.lcc.frame.Advertisements;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Author:  梁铖城
@@ -17,10 +23,37 @@ import com.lcc.activity.R;
  */
 public class HomeFragment extends Fragment {
 
+    private LinearLayout llAdvertiseBoard;
+    private LayoutInflater inflaters;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, null);
+        inflaters = LayoutInflater.from(getActivity());
+        llAdvertiseBoard = (LinearLayout) view.findViewById(R.id.llAdvertiseBoard);
+        initViews();
         return  view;
+    }
+
+    private void initViews() {
+        JSONArray advertiseArray = new JSONArray();
+        try {
+            JSONObject head_img0 = new JSONObject();
+            head_img0.put("head_img", "http://www.17yxb.cn/image/m1.png");
+            JSONObject head_img1 = new JSONObject();
+            head_img1.put("head_img", "http://www.17yxb.cn/image/m2.png");
+            JSONObject head_img2 = new JSONObject();
+            head_img2.put("head_img", "http://www.17yxb.cn/image/m3.png");
+            JSONObject head_img3 = new JSONObject();
+            head_img3.put("head_img", "http://www.17yxb.cn/image/m4.png");
+            advertiseArray.put(head_img0);
+            advertiseArray.put(head_img1);
+            advertiseArray.put(head_img2);
+            advertiseArray.put(head_img3);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        llAdvertiseBoard.addView(new Advertisements(getActivity(), true, inflaters, 3000).initView(advertiseArray));
     }
 }
---
+++ app/src/main/java/com/lcc/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.lcc.constants;
+
+/**
+ * Author:  梁铖城
+ * Email:   1038127753@qq.com
+ * Date:    2015年12月15日10:47:52
+ * Description:   AppConstants
+ */
+public class AppConstants {
+
+    public final static String BASE_URL = "https://192.168.0.1";
+}
