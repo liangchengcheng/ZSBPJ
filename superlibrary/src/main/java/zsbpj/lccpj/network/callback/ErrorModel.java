a15be19dd45f6078fa353f148cf32f26acf1a826
chengcheng <1038127753@qq.com>
2016-01-13 23:24:30 +0800

    开始封装

---
+++ superlibrary/src/main/java/zsbpj/lccpj/network/callback/ErrorModel.java
@@ -0,0 +1,23 @@
+package zsbpj.lccpj.network.callback;
+
+public class ErrorModel {
+    public int mStatusCode;
+    public String mMessage;
+
+    public ErrorModel() {
+
+    }
+
+    public ErrorModel(int statusCode, String msg) {
+        this.mStatusCode = statusCode;
+        this.mMessage = msg;
+    }
+
+    public String getMessage() {
+        return mMessage == null ? "" : mMessage;
+    }
+
+    public int getStatusCode() {
+        return mStatusCode;
+    }
+}
