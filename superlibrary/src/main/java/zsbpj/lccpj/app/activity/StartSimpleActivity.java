05ce2c3ed37ede109d8931911a2f5b1a3285ecbb
chengcheng <1038127753@qq.com>
2016-01-17 18:32:03 +0800

    提交

--- superlibrary/src/main/java/zsbpj/lccpj/app/activity/StartSimpleActivity.java
+++ superlibrary/src/main/java/zsbpj/lccpj/app/activity/StartSimpleActivity.java
@@ -9,7 +9,7 @@ import butterknife.ButterKnife;
  * Author:  梁铖城
  * Email:   1038127753@qq.com
  * Date:    2015年12月15日10:47:52
- * Description:
+ * Description: StartSimpleActivity
  */
 public abstract class StartSimpleActivity extends AppCompatActivity {
 
