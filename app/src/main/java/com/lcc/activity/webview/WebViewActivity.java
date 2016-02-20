b2e6579660705db3c1afcd751329170eaac71bdf
liangchengcheng <1038127753@qq.com>
2016-02-20 20:46:44 +0800

    增加了webview的activity

---
+++ app/src/main/java/com/lcc/activity/webview/WebViewActivity.java
@@ -0,0 +1,132 @@
+package com.lcc.activity.webview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import com.lcc.activity.R;
+import com.lcc.utils.ResourcesUtils;
+
+/**
+ * 应该继承baseactivity
+ */
+public class WebViewActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+
+    private static final String KEY_URL="url";
+    private Toolbar mToolbar;
+    private String url;
+    private WebView webView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ProgressBar progressBar;
+
+    public static Intent createIntent(Context context, String url) {
+        Intent intent = new Intent(context, WebViewActivity.class);
+        intent.putExtra(KEY_URL, url);
+        return intent;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        initToolBar();
+        initWebView();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        int color = ResourcesUtils.getColor(this, R.color.colorPrimary);
+        progressBar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        url = getIntent().getStringExtra(KEY_URL);
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onRefresh() {
+        webView.reload();
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void initToolBar(){
+        mToolbar= (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(mToolbar);
+        toolbarAsBackButton(mToolbar);
+    }
+
+    public void toolbarAsBackButton(Toolbar toolbar) {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode==KeyEvent.KEYCODE_BACK){
+            if (webView.canGoBack()){
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void initWebView(){
+        webView= (WebView) findViewById(R.id.webview);
+        WebSettings setting=webView.getSettings();
+        setting.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        setting.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+                view.loadUrl(url);
+                return true;
+            }
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                super.onReceivedHttpError(view, request, errorResponse);
+            }
+
+        });
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress!=100){
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                mToolbar.setTitle(title);
+            }
+        });
+    }
+}
