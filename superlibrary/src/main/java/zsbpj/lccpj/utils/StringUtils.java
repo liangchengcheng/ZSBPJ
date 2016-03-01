2600ba41c2db55181aee5ecfb8b78854a005c2a7
liangchengcheng <1038127753@qq.com>
2016-03-01 22:57:41 +0800

    增加城市选择的基本的sql

---
+++ superlibrary/src/main/java/zsbpj/lccpj/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package zsbpj.lccpj.utils;
+
+public class StringUtils {
+
+    /**
+     * 提取出城市或者县
+     */
+    public static String extractLocation(final String city, final String district){
+        return district.contains("县") ? district.substring(0, district.length() - 1) : city.substring(0, city.length() - 1);
+    }
+}
