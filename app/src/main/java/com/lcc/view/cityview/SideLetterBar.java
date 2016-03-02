b4e4acbf0673ee316db76bbf1c8a39bc8796bc02
liangchengcheng <1038127753@qq.com>
2016-03-02 20:51:36 +0800

    增加城市选择的view

---
+++ app/src/main/java/com/lcc/view/cityview/SideLetterBar.java
@@ -0,0 +1,133 @@
+package com.lcc.view.cityview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lcc.activity.R;
+
+public class SideLetterBar extends View {
+
+    private static final String[] b = {"定位", "热门", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private int choose = -1;
+    private Paint paint = new Paint();
+    private boolean showBg = false;
+    private OnLetterChangedListener onLetterChangedListener;
+    private TextView overlay;
+
+    public SideLetterBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SideLetterBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SideLetterBar(Context context) {
+        super(context);
+    }
+
+    /**
+     * 设置悬浮的overlay
+     *
+     * @param overlay overlay
+     */
+    public void setOverlay(TextView overlay) {
+        this.overlay = overlay;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (showBg) {
+            canvas.drawColor(Color.TRANSPARENT);
+        }
+        int height = getHeight();
+        int width = getWidth();
+        //单个条目的高度
+        int singleHeight = height / b.length;
+        for (int i = 0; i < b.length; i++) {
+            paint.setTextSize(getResources().getDimension(R.dimen.side_letter_bar_letter_size));
+            paint.setColor(getResources().getColor(R.color.gray));
+            paint.setAntiAlias(true);
+            if (i == choose) {
+                paint.setColor(getResources().getColor(R.color.gray_deep));
+            }
+            float xPos = width / 2 - paint.measureText(b[i]) / 2;
+            float yPos = singleHeight * i + singleHeight;
+            canvas.drawText(b[i],xPos,yPos,paint);
+            paint.reset();
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+        final float y = event.getY();
+        final int oldChoose = choose;
+        final OnLetterChangedListener listener = onLetterChangedListener;
+        final int c = (int) (y / getHeight() * b.length);
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                showBg = true;
+                if (oldChoose != c && listener != null) {
+                    if (c >= 0 && c < b.length) {
+                        listener.onLetterChanged(b[c]);
+                        choose = c;
+                        invalidate();
+                        if (overlay != null){
+                            overlay.setVisibility(VISIBLE);
+                            overlay.setText(b[c]);
+                        }
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (oldChoose != c && listener != null) {
+                    if (c >= 0 && c < b.length) {
+                        listener.onLetterChanged(b[c]);
+                        choose = c;
+                        invalidate();
+                        if (overlay != null){
+                            overlay.setVisibility(VISIBLE);
+                            overlay.setText(b[c]);
+                        }
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                showBg = false;
+                choose = -1;
+                invalidate();
+                if (overlay != null){
+                    overlay.setVisibility(GONE);
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+
+    public void setOnLetterChangedListener(OnLetterChangedListener onLetterChangedListener) {
+        this.onLetterChangedListener = onLetterChangedListener;
+    }
+
+    /**
+     * 右侧的字母的点击事件
+     */
+    public interface OnLetterChangedListener {
+        void onLetterChanged(String letter);
+    }
+}
