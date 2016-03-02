157342931d6ac52ce2d62aa5229376c324b1b2f9
liangchengcheng <1038127753@qq.com>
2016-03-02 21:54:12 +0800

    增加城市选择的activity

---
+++ app/src/main/java/com/lcc/activity/main/activity/CityPickerActivity.java
@@ -0,0 +1,208 @@
+package com.lcc.activity.main.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.lcc.activity.R;
+import com.lcc.adapter.CityListAdapter;
+import com.lcc.adapter.ResultListAdapter;
+import com.lcc.bean.City;
+import com.lcc.bean.LocateState;
+import com.lcc.bean.News;
+import com.lcc.frame.sql.CityDBManager;
+import com.lcc.rx.RxService;
+import com.lcc.service.ApiService;
+import com.lcc.view.cityview.SideLetterBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import zsbpj.lccpj.app.activity.StartNetActivity;
+import zsbpj.lccpj.frame.FrameManager;
+import zsbpj.lccpj.utils.StringUtils;
+
+/**
+ * Author:  梁铖城
+ * Email:   1038127753@qq.com
+ * Date:    2015年12月15日10:47:52
+ * Description:    {  }
+ */
+public class CityPickerActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ListView mListView;
+    private ListView mResultListView;
+    private SideLetterBar mLetterBar;
+    private EditText searchBox;
+    private ImageView clearBtn;
+    private ImageView backBtn;
+    private ViewGroup emptyView;
+
+    private CityListAdapter mCityAdapter;
+    private ResultListAdapter mResultAdapter;
+    private List<City> mAllCities;
+    private CityDBManager dbManager;
+
+    private AMapLocationClient mLocationClient;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_list);
+
+        initData();
+        initView();
+        initLocation();
+    }
+
+    /**
+     * 初始化定位相关
+     */
+    private void initLocation() {
+        mLocationClient = new AMapLocationClient(this);
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        option.setOnceLocation(true);
+        mLocationClient.setLocationOption(option);
+        mLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        String city = aMapLocation.getCity();
+                        String district = aMapLocation.getDistrict();
+                        String location = StringUtils.extractLocation(city, district);
+                        mCityAdapter.updateLocateState(LocateState.SUCCESS, location);
+                    } else {
+                        //定位失败
+                        mCityAdapter.updateLocateState(LocateState.FAILED, null);
+                    }
+                }
+            }
+        });
+        mLocationClient.startLocation();
+    }
+
+    private void initView() {
+        mListView = (ListView) findViewById(R.id.listview_all_city);
+        mListView.setAdapter(mCityAdapter);
+
+        TextView overlay = (TextView) findViewById(R.id.tv_letter_overlay);
+        mLetterBar = (SideLetterBar) findViewById(R.id.side_letter_bar);
+        mLetterBar.setOverlay(overlay);
+        mLetterBar.setOnLetterChangedListener(new SideLetterBar.OnLetterChangedListener() {
+            @Override
+            public void onLetterChanged(String letter) {
+                int position = mCityAdapter.getLetterPosition(letter);
+                mListView.setSelection(position);
+            }
+        });
+
+        searchBox = (EditText) findViewById(R.id.et_search);
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String keyword = s.toString();
+                if (TextUtils.isEmpty(keyword)) {
+                    clearBtn.setVisibility(View.GONE);
+                    emptyView.setVisibility(View.GONE);
+                    mResultListView.setVisibility(View.GONE);
+                } else {
+                    clearBtn.setVisibility(View.VISIBLE);
+                    mResultListView.setVisibility(View.VISIBLE);
+                    List<City> result = dbManager.searchCity(keyword);
+                    if (result == null || result.size() == 0) {
+                        emptyView.setVisibility(View.VISIBLE);
+                    } else {
+                        emptyView.setVisibility(View.GONE);
+                        mResultAdapter.changeData(result);
+                    }
+                }
+            }
+        });
+
+        emptyView = (ViewGroup) findViewById(R.id.empty_view);
+        mResultListView = (ListView) findViewById(R.id.listview_search_result);
+        mResultListView.setAdapter(mResultAdapter);
+        mResultListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                back(mResultAdapter.getItem(position).getName());
+            }
+        });
+
+        clearBtn = (ImageView) findViewById(R.id.iv_search_clear);
+        backBtn = (ImageView) findViewById(R.id.back);
+
+        clearBtn.setOnClickListener(this);
+        backBtn.setOnClickListener(this);
+    }
+
+    private void back(String city){
+        FrameManager.getInstance().toastPrompt(city);
+    }
+
+    private void initData() {
+        dbManager = new CityDBManager(this);
+        dbManager.copyDBFile();
+        mAllCities = dbManager.getAllCities();
+        mCityAdapter = new CityListAdapter(this, mAllCities);
+        mCityAdapter.setOnCityClickListener(new CityListAdapter.OnCityClickListener() {
+            @Override
+            public void onCityClick(String name) {
+                back(name);
+            }
+
+            @Override
+            public void onLocateClick() {
+                mCityAdapter.updateLocateState(LocateState.LOCATING, null);
+                mLocationClient.startLocation();
+            }
+        });
+
+        mResultAdapter = new ResultListAdapter(this, null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_search_clear:
+                searchBox.setText("");
+                clearBtn.setVisibility(View.GONE);
+                emptyView.setVisibility(View.GONE);
+                mResultListView.setVisibility(View.GONE);
+                break;
+            case R.id.back:
+                finish();
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stopLocation();
+    }
+}
