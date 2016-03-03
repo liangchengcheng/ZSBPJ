94aeb004f0b37589824ed1ddc75668a840c2096d
liangchengcheng <1038127753@qq.com>
2016-03-03 21:48:37 +0800

    添加视频相关

--- app/src/main/java/com/lcc/activity/main/fragment/OnlineClassFragment.java
+++ app/src/main/java/com/lcc/activity/main/fragment/OnlineClassFragment.java
@@ -1,14 +1,22 @@
 package com.lcc.activity.main.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lcc.activity.MainActivity;
 import com.lcc.activity.R;
+import com.lcc.adapter.MediasAdapter;
+import com.lcc.entity.MediaEntity;
 
+import java.util.List;
+
+import zsbpj.lccpj.view.recyclerview.MGridLayoutManager;
 import zsbpj.lccpj.view.recyclerview.RefreshAndLoadFragment;
 
 /**
@@ -17,17 +25,93 @@ import zsbpj.lccpj.view.recyclerview.RefreshAndLoadFragment;
  * Date:    2015年12月15日10:47:52
  * Description:  在线课程
  */
-public class OnlineClassFragment extends RefreshAndLoadFragment {
+public class OnlineClassFragment extends RefreshAndLoadFragment implements MediasAdapter.OnItemClickListener  {
 
-    @Nullable
+
+    private static final String KEY_VIDEO_ID = "id";
+    private static final String KEY_VIDEO_TYPE = "type";
+
+    private MediasAdapter mAdapter;
+    private MainActivity mainActivity;
+    private RecyclerView mRecyclerView;
+
+    private static final int PAGER_SIZE = 20;
+    private static final int GRID_COLUMN = 2;
+
+    private int id;
+    private int type;
+
+    /**
+     * 初始化这个fragment
+     */
+    public static Fragment newInstance(int id,int type){
+        Bundle bundle=new Bundle();
+        bundle.putInt(KEY_VIDEO_ID,id);
+        bundle.putInt(KEY_VIDEO_TYPE,type);
+        Fragment fragment=new OnlineClassFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_online, null);
-        return  view;
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mainActivity= (MainActivity) context;
     }
 
     @Override
     public void onRefreshData() {
+
+    }
+
+    @Override
+    protected void onFragmentLoadMore() {
+        super.onFragmentLoadMore();
+    }
+
+    @Override
+    protected void onFragmentCreate() {
+        super.onFragmentCreate();
+        id=getArguments().getInt(KEY_VIDEO_ID);
+        type=getArguments().getInt(KEY_VIDEO_TYPE);
+        mRecyclerView=getRecyclerView();
+        mRecyclerView.setHasFixedSize(true);
+        mAdapter=new MediasAdapter(getActivity());
+        setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(this);
+        mAdapter.setHasMoreData(true);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new MGridLayoutManager(getActivity(), GRID_COLUMN, mAdapter));
+
+    }
+
+    private void autoRefresh(){
+        getSwipeRefreshWidget().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //设置当前的状态是刷新
+                currentPage=STATE_REFRESH;
+                getSwipeRefreshWidget().setRefreshing(true);
+                // TODO: 16/3/3 在此处加载数据 
+                //mPresenter.refresh(id, type, PAGER_SIZE);
+            }
+        },500);
+    }
+
+    void showError(){
+
+    }
+
+    void refreshView(List<MediaEntity> entities){
+
+    }
+
+    void loadMoreView(List<MediaEntity> entities){
+
+    }
+
+    @Override
+    public void OnItemClick(MediaEntity entity) {
 
     }
 }
--- app/src/main/java/com/lcc/adapter/MediasAdapter.java
+++ app/src/main/java/com/lcc/adapter/MediasAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.lcc.activity.R;
 import com.lcc.entity.MediaEntity;
