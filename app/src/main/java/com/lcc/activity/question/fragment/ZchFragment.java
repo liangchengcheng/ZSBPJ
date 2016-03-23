11ddf3b26ae26b1db1c23f4f7808e71a3b25a822
liangchengcheng <1038127753@qq.com>
2016-03-23 14:45:25 +0800

    主页的布局之子view

--- app/src/main/java/com/lcc/activity/question/fragment/ZchFragment.java
+++ app/src/main/java/com/lcc/activity/question/fragment/ZchFragment.java
@@ -3,28 +3,118 @@ package com.lcc.activity.question.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lcc.activity.R;
+import com.lcc.activity.video.VideoPlayActivity;
+import com.lcc.activity.video.VideoUtils;
+import com.lcc.adapter.MediasAdapter;
+import com.lcc.adapter.ZXAdapter;
+import com.lcc.constants.StateConstants;
+import com.lcc.entity.MediaEntity;
+import com.lcc.entity.ResultEntity;
+import com.lcc.entity.VideoItemEntity;
+import com.lcc.entity.ZXTest;
+import com.lcc.rx.RxService;
 
-/**
- * Created by lcc on 16/3/17.
- */
-public class ZchFragment extends Fragment{
+import java.util.ArrayList;
+import java.util.List;
+
+import de.greenrobot.event.EventBus;
+import zsbpj.lccpj.frame.FrameManager;
+import zsbpj.lccpj.utils.LogUtils;
+import zsbpj.lccpj.view.recyclerview.MGridLayoutManager;
+import zsbpj.lccpj.view.recyclerview.RefreshAndLoadFragment;
+
+public class ZchFragment extends RefreshAndLoadFragment implements ZXAdapter.OnItemClickListener{
 
     public static Fragment newInstance() {
         Fragment fragment = new ZchFragment();
         return fragment;
     }
 
+    private ZXAdapter mAdapter;
+    static final int ACTION_NONE = 0;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_zc,null);
-        return view;
+    public void onRefreshData() {
+        showRefreshData(getData());
     }
 
+    @Override
+    protected void onFragmentLoadMore() {
+        showMoreData(getData());
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onFragmentCreate() {
+        super.onFragmentCreate();
+
+        RecyclerView mRecyclerView = getRecyclerView();
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new ZXAdapter(getActivity());
+        setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(this);
+        mAdapter.setHasMoreData(true);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        autoRefresh();
+    }
+
+    /**
+     * 刷新数据
+     */
+    private void autoRefresh() {
+        getSwipeRefreshWidget().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                currentPage = STATE_REFRESH;
+                getSwipeRefreshWidget().setRefreshing(true);
+                showRefreshData(getData());
+            }
+        }, 500);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void showError() {
+        currentState = ACTION_NONE;
+        if (getSwipeRefreshWidget().isRefreshing()) {
+            getSwipeRefreshWidget().setRefreshing(false);
+            FrameManager.getInstance().toastPrompt("刷新数据失败");
+        } else {
+            FrameManager.getInstance().toastPrompt("加载数据失败");
+            mAdapter.setHasFooter(false);
+        }
+    }
+
+    @Override
+    public void OnItemClick(ZXTest entity) {
+
+    }
+
+    private List<ZXTest> getData(){
+        List<ZXTest> data=new ArrayList<>();
+        for (int i=0;i<10;i++){
+            ZXTest zxTest=new ZXTest();
+            zxTest.setDate("2016年03月23日14:26:29"+i);
+            zxTest.setName("美国挑衅南海，中国外交部强烈谴责");
+            zxTest.setJj("浏览器根据请求的URL交给DNS域名解析，找到真实IP，向服务器发起请求服务器交给后台处理完成后返回数据，浏览器接收文件（HTML、JS、CSS、图象等）浏览器对加载到的资源（HTML、JS、CSS等）进行语法解析，建立相应的内部数据结构（如HTML的DOM）");
+            data.add(zxTest);
+        }
+        return data;
+    }
 }
---
+++ app/src/main/java/com/lcc/adapter/ZXAdapter.java
@@ -0,0 +1,72 @@
+package com.lcc.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.lcc.activity.R;
+import com.lcc.entity.MediaEntity;
+import com.lcc.entity.ZXTest;
+import com.lcc.view.ShadowImageView;
+import com.lcc.view.TextImageView;
+
+import zsbpj.lccpj.view.recyclerview.adapter.LoadMoreRecyclerAdapter;
+
+public class ZXAdapter extends LoadMoreRecyclerAdapter<ZXTest,ZXAdapter.ViewHolder>{
+
+    private  OnItemClickListener onItemClickListener;
+    private Activity mActivity;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener=onItemClickListener;
+    }
+
+    public ZXAdapter(Activity activity){
+        this.mActivity=activity;
+    }
+
+    @Override
+    public ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.zxtest_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindItemViewHolder(ViewHolder holder, final int position) {
+        ZXTest entity=getItem(position);
+        holder.ll_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.OnItemClick(getItem(position));
+            }
+        });
+        holder.tv_title.setText(entity.getName());
+        holder.tv_content.setText(entity.getJj());
+        holder.tv_time.setText(entity.getDate());
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public final TextView tv_title;
+        public final TextView tv_content;
+        public final TextView tv_time;
+        public final LinearLayout ll_all;
+
+        public ViewHolder(View view) {
+            super(view);
+            tv_title = (TextView) view.findViewById(R.id.tv_title);
+            tv_content = (TextView) view.findViewById(R.id.tv_content);
+            tv_time = (TextView) view.findViewById(R.id.tv_time);
+            ll_all = (LinearLayout) view.findViewById(R.id.ll_all);
+        }
+    }
+
+    public interface OnItemClickListener{
+        void OnItemClick(ZXTest entity);
+    }
+}
