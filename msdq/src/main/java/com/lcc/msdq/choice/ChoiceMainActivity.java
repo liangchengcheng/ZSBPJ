7921413ee34b97d878b0c2dd3b90479752a9bc17
liangchengcheng <1038127753@qq.com>
2016-04-22 07:28:34 +0800

    增加职业选择界面

---
+++ msdq/src/main/java/com/lcc/msdq/choice/ChoiceMainActivity.java
@@ -0,0 +1,25 @@
+package com.lcc.msdq.choice;
+
+import com.lcc.base.BaseActivity;
+import com.lcc.msdq.R;
+
+/**
+ * Created by lcc on 16/4/22.
+ */
+public class ChoiceMainActivity extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected boolean Open() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.activity_choice;
+    }
+}
