6adf5184cf0e1f0b454f9a9be8b251b9caf8f09f
liangchengcheng <1038127753@qq.com>
2016-04-23 06:05:38 +0800

    细节调整

--- msdq/src/main/java/com/lcc/msdq/choice/ChoiceMainActivity.java
+++ msdq/src/main/java/com/lcc/msdq/choice/ChoiceMainActivity.java
@@ -3,25 +3,24 @@ package com.lcc.msdq.choice;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-
 import com.lcc.adapter.ChoiceAdapter;
 import com.lcc.base.BaseActivity;
 import com.lcc.msdq.R;
 import java.util.Arrays;
 
-
 public class ChoiceMainActivity extends BaseActivity {
 
     private String[] name = {"编辑","会计","文员","产品经理","人事","客服","销售","平面设计","程序员"};
-
+    private ChoiceAdapter adapter;
     @Override
     protected void initView() {
         GridView girdview = (GridView) this.findViewById(R.id.gv_gz);
-        girdview.setAdapter(new ChoiceAdapter(ChoiceMainActivity.this, Arrays.asList(name)));
+        adapter=new ChoiceAdapter(ChoiceMainActivity.this, Arrays.asList(name));
+        girdview.setAdapter(adapter);
         girdview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                adapter.setSelectItem(position);
             }
         });
     }
