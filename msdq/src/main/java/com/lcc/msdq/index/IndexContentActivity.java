590bc72bb3b5829969d2d85908fa389e6f1a7b91
liangchengcheng <1038127753@qq.com>
2016-05-18 22:35:40 +0800

    修改首页的内容

---
+++ msdq/src/main/java/com/lcc/msdq/index/IndexContentActivity.java
@@ -0,0 +1,181 @@
+package com.lcc.msdq.index;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.PopupWindow;
+import android.widget.ProgressBar;
+import com.lcc.base.BaseActivity;
+import com.lcc.msdq.R;
+import com.lcc.utils.ResourcesUtils;
+
+/**
+ * 应该继承baseactivity
+ */
+public class IndexContentActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private static final String KEY_URL = "url";
+    private Toolbar mToolbar;
+    private String url;
+    private WebView webView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+        initToolBar();
+        initWebView();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        int color = ResourcesUtils.getColor(this, R.color.colorPrimary);
+        progressBar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        url = getIntent().getStringExtra(KEY_URL);
+        webView.loadUrl(url);
+    }
+
+    @Override
+    protected boolean Open() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.activity_webview;
+    }
+
+    @Override
+    public void onRefresh() {
+        webView.reload();
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void initToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        toolbarAsBackButton(mToolbar);
+    }
+
+    public void toolbarAsBackButton(Toolbar toolbar) {
+        toolbar.setBackgroundColor(getColorPrimary());
+        toolbar.setTitle("内容展示");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        findViewById(R.id.iv_share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // showPopwindow();
+            }
+        });
+    }
+
+//    /**
+//     * 显示popupWindow
+//     */
+//    private void showPopwindow() {
+//        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View view = inflater.inflate(R.layout.dialog_layout, null);
+//        //下面是两种方法得到宽度和高度 getWindow().getDecorView().getWidth()
+//        PopupWindow window = new PopupWindow(view,
+//                WindowManager.LayoutParams.MATCH_PARENT,
+//                WindowManager.LayoutParams.WRAP_CONTENT);
+//        window.setFocusable(true);
+//        ColorDrawable dw = new ColorDrawable(0xb0000000);
+//        window.setBackgroundDrawable(dw);
+//        window.setAnimationStyle(R.style.mypopwindow_anim_style);
+//        window.showAtLocation(WebViewActivity.this.findViewById(R.id.root_View),
+//                Gravity.BOTTOM, 0, 0);
+//        window.setOnDismissListener(new PopupWindow.OnDismissListener() {
+//
+//            @Override
+//            public void onDismiss() {
+//            }
+//        });
+//    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void initWebView() {
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings setting = webView.getSettings();
+        setting.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        setting.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                super.onReceivedHttpError(view, request, errorResponse);
+            }
+
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress != 100) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                mToolbar.setTitle(title);
+            }
+        });
+    }
+}
