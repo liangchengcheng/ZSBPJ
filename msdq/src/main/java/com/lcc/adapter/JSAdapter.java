4112f26979b6055cca0ebe40d8f07bff30662ce8
liangchengcheng <1038127753@qq.com>
2016-06-10 12:09:51 +0800

    添加公司的面试资料的问题的列表

---
+++ msdq/src/main/java/com/lcc/adapter/JSAdapter.java
@@ -0,0 +1,222 @@
+package com.lcc.adapter;
+
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.lcc.entity.Article;
+import com.lcc.entity.CompanyTest;
+import com.lcc.msdq.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import zsbpj.lccpj.frame.ImageManager;
+
+/**
+ * Author:       梁铖城
+ * Email:        1038127753@qq.com
+ * Date:         2015年11月21日15:28:25
+ * Description:  JSAdapter
+ */
+public class JSAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int NORMAL_ITEM = 0;
+    public static final int FOOTER_ITEM = 2;
+
+    private List<CompanyTest> mList = new ArrayList<>();
+
+    /**
+     * 是否设置了footer
+     */
+    private boolean hasFooter;
+
+    /**
+     * 是否继续加载数据
+     */
+    private boolean hasMoreData = true;
+
+    public void bind(List<CompanyTest> messages) {
+        this.mList = messages;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getBasicItemCount() && hasFooter) {
+            return FOOTER_ITEM;
+        } else {
+            return NORMAL_ITEM;
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == NORMAL_ITEM) {
+            return new NormalViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.js_item, parent, false));
+        } else {
+            return new FootViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_foot_loading, parent, false));
+        }
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        if (viewHolder instanceof FootViewHolder) {
+            if (hasMoreData) {
+                ((FootViewHolder) viewHolder).mProgressView.setVisibility(View.VISIBLE);
+                ((FootViewHolder) viewHolder).mTextView.setText("正在加载...");
+            } else {
+                ((FootViewHolder) viewHolder).mProgressView.setVisibility(View.GONE);
+                ((FootViewHolder) viewHolder).mTextView.setText("没有更多数据...");
+            }
+        } else {
+            final CompanyTest weekData = mList.get(position);
+            NormalViewHolder holder = (NormalViewHolder) viewHolder;
+            holder.tv_title.setText(weekData.getTitle());
+            holder.tv_summary.setText(weekData.getSummary());
+//            if (TextUtils.isEmpty(weekData.getImage_url())) {
+//                holder.iv_head.setVisibility(View.GONE);
+//            } else {
+//                holder.iv_head.setVisibility(View.VISIBLE);
+//                ImageManager.getInstance().loadUrlImage(holder.iv_head.getContext(),
+//                        weekData.getImage_url(), holder.iv_head);
+//            }
+//
+//            if (weekData.getState().equals("0")) {
+//                holder.state.setVisibility(View.GONE);
+//            } else {
+//                holder.state.setVisibility(View.VISIBLE);
+//                if (weekData.getState().equals("1")) {
+//                    holder.state.setText("热门");
+//                    holder.state.setTextColor(Color.parseColor("#FFFF0000"));
+//                }
+//                if (weekData.getState().equals("2")) {
+//                    holder.state.setText("置顶");
+//                    holder.state.setTextColor(Color.parseColor("#388E3C"));
+//                }
+//            }
+
+            if (mListener != null) {
+                holder.ll_all.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mListener.onItemClick(weekData);
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return getBasicItemCount() + (hasFooter ? 1 : 0);
+
+    }
+
+    public int getBasicItemCount() {
+        return mList.size();
+    }
+
+    /**
+     * 正常的布局
+     */
+    class NormalViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.iv_head)
+        ImageView iv_head;
+
+        @Bind(R.id.tv_title)
+        TextView tv_title;
+
+        @Bind(R.id.tv_summary)
+        TextView tv_summary;
+
+        @Bind(R.id.ll_all)
+        CardView ll_all;
+
+        @Bind(R.id.state)
+        TextView state;
+
+        public NormalViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    /**
+     * 头部的布局
+     */
+    class FootViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.mProgressView)
+        ProgressBar mProgressView;
+
+        @Bind(R.id.mTextView)
+        TextView mTextView;
+
+        public FootViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public void appendToList(List<CompanyTest> list) {
+        if (list == null) {
+            return;
+        }
+        mList.addAll(list);
+    }
+
+    public List<CompanyTest> getList() {
+        return mList;
+    }
+
+    public void setHasFooter(boolean hasFooter) {
+        if (this.hasFooter != hasFooter) {
+            this.hasFooter = hasFooter;
+            notifyDataSetChanged();
+        }
+    }
+
+    public boolean hasMoreData() {
+        return hasMoreData;
+    }
+
+    public void setHasMoreData(boolean isMoreData) {
+        if (this.hasMoreData != isMoreData) {
+            this.hasMoreData = isMoreData;
+            notifyDataSetChanged();
+        }
+    }
+
+    public void setHasMoreDataAndFooter(boolean hasMoreData, boolean hasFooter) {
+        if (this.hasMoreData != hasMoreData || this.hasFooter != hasFooter) {
+            this.hasMoreData = hasMoreData;
+            this.hasFooter = hasFooter;
+            notifyDataSetChanged();
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(CompanyTest data);
+    }
+
+    private OnItemClickListener mListener;
+
+    public void setOnItemClickListener(OnItemClickListener li) {
+        this.mListener = li;
+    }
+}
--- msdq/src/main/java/com/lcc/msdq/compony/content/CodeFragment.java
+++ msdq/src/main/java/com/lcc/msdq/compony/content/CodeFragment.java
@@ -1,89 +1,177 @@
 package com.lcc.msdq.compony.content;
 
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.lcc.adapter.CodeAdapter;
+import com.lcc.adapter.IndexMenuAdapter;
+import com.lcc.adapter.JSAdapter;
+import com.lcc.entity.Article;
 import com.lcc.entity.CodeEntity;
+import com.lcc.entity.CompanyTest;
+import com.lcc.frame.fragment.base.BaseLazyLoadFragment;
+import com.lcc.msdq.R;
+import com.lcc.msdq.index.IndexMenuView.IndexMenuWebView;
+import com.lcc.mvp.presenter.IndexMenuPresenter;
+import com.lcc.mvp.presenter.JSPresenter;
+import com.lcc.mvp.presenter.impl.IndexMenuPresenterImpl;
+import com.lcc.mvp.presenter.impl.JSPresenterImpl;
+import com.lcc.mvp.view.JSView;
+import com.lcc.view.loadview.LoadingLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import zsbpj.lccpj.frame.FrameManager;
+import zsbpj.lccpj.utils.TimeUtils;
 import zsbpj.lccpj.view.recyclerview.RefreshAndLoadFragment;
+import zsbpj.lccpj.view.recyclerview.listener.OnRecycleViewScrollListener;
 
-public class CodeFragment extends RefreshAndLoadFragment implements CodeAdapter.OnItemClickListener {
+public class CodeFragment extends BaseLazyLoadFragment implements
+        SwipeRefreshLayout.OnRefreshListener,JSView {
 
-    private CodeAdapter mAdapter;
     static final int ACTION_NONE = 0;
+    protected static final int DEF_DELAY = 1000;
+    protected final static int STATE_LOAD = 0;
+    protected final static int STATE_NORMAL = 1;
+    protected int currentState = STATE_NORMAL;
+    protected long currentTime = 0;
+    protected int currentPage = 1;
+
+    private JSAdapter adapter;
+    private LoadingLayout loading_layout;
+    private SwipeRefreshLayout mSwipeRefreshWidget;
+    private RecyclerView mRecyclerView;
+    private JSPresenter mPresenter;
+    private String fid="1cddd741560e7d90ebf9112b989ba955";
+    private String type;
 
     @Override
-    public void onRefreshData() {
-        showRefreshData(getData());
+    public int initContentView() {
+        return R.layout.code_fragment;
     }
 
     @Override
-    protected void onFragmentLoadMore() {
-        showMoreData(getData());
+    public void getBundle(Bundle bundle) {
+
     }
 
     @Override
-    protected void onFragmentCreate() {
-        super.onFragmentCreate();
-        RecyclerView mRecyclerView = getRecyclerView();
-        mRecyclerView.setHasFixedSize(true);
+    public void initUI(View view) {
+        loading_layout = (LoadingLayout) view.findViewById(R.id.loading_layout);
+        mPresenter = new JSPresenterImpl(this);
+        initRefreshView(view);
+        initRecycleView(view);
 
-        mAdapter = new CodeAdapter(getActivity());
-        setAdapter(mAdapter);
-        mAdapter.setOnItemClickListener(this);
-        mAdapter.setHasMoreData(true);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        autoRefresh();
     }
 
-    public void showError() {
-        currentState = ACTION_NONE;
-        if (getSwipeRefreshWidget().isRefreshing()) {
-            getSwipeRefreshWidget().setRefreshing(false);
-            FrameManager.getInstance().toastPrompt("刷新数据失败");
-        } else {
-            FrameManager.getInstance().toastPrompt("加载数据失败");
-            mAdapter.setHasFooter(false);
-        }
+    private void initRefreshView(View view) {
+        mSwipeRefreshWidget = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_widget);
+        mSwipeRefreshWidget.setColorSchemeResources(R.color.colorPrimary);
+        mSwipeRefreshWidget.setOnRefreshListener(this);
     }
 
+    private void initRecycleView(View view) {
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new JSAdapter();
 
-    /**
-     * 刷新数据
-     */
-    private void autoRefresh() {
-        getSwipeRefreshWidget().postDelayed(new Runnable() {
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.addOnScrollListener(new OnRecycleViewScrollListener() {
+            @Override
+            public void onLoadMore() {
+                if (currentState == STATE_NORMAL) {
+                    currentState = STATE_LOAD;
+                    currentTime = TimeUtils.getCurrentTime();
+                    adapter.setHasFooter(true);
+                    mRecyclerView.scrollToPosition(adapter.getItemCount() - 1);
+                    currentPage++;
+                    mPresenter.loadMore(currentPage,fid,type);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+        mPresenter.getData(1,fid,type);
+    }
+
+    @Override
+    public void onRefresh() {
+        mSwipeRefreshWidget.postDelayed(new Runnable() {
             @Override
             public void run() {
-                currentPage = STATE_REFRESH;
-                getSwipeRefreshWidget().setRefreshing(true);
-                showRefreshData(getData());
+                currentPage = 1;
+                mSwipeRefreshWidget.setRefreshing(true);
+                mPresenter.refresh(currentPage,fid,type);
             }
         }, 500);
     }
 
     @Override
-    public void OnItemClick(CodeEntity entity) {
-
+    public void getLoading() {
+        loading_layout.setLoadingLayout(LoadingLayout.NETWORK_LOADING);
     }
 
-    private List<CodeEntity> getData(){
-        List<CodeEntity> data=new ArrayList<>();
-        for (int i=0;i<10;i++){
-            CodeEntity zxTest=new CodeEntity();
-            zxTest.setDate("2016年03月23日14:26:29"+i);
-            zxTest.setName("SD学院公布了最新的录取分数线");
-            zxTest.setJj("浏览器根据请求的URL交给DNS域名解析，找到真实IP，向服务器发起请求服务器交给后台处理完成后返回数据，浏览器接收文件（HTML、JS、CSS、图象等）浏览器对加载到的资源（HTML、JS、CSS等）进行语法解析，建立相应的内部数据结构（如HTML的DOM）");
-            data.add(zxTest);
+    @Override
+    public void getDataEmpty() {
+        loading_layout.setLoadingLayout(LoadingLayout.NO_DATA);
+    }
+
+    @Override
+    public void getDataFail(String msg) {
+        loading_layout.setLoadingLayout(LoadingLayout.LOADDATA_ERROR);
+    }
+
+    @Override
+    public void refreshOrLoadFail(String msg) {
+        if (mSwipeRefreshWidget.isRefreshing()) {
+            mSwipeRefreshWidget.setRefreshing(false);
+            loading_layout.setLoadingLayout(LoadingLayout.LOADDATA_ERROR);
+        } else {
+            FrameManager.getInstance().toastPrompt(msg);
         }
-        return data;
+    }
+
+    @Override
+    public void refreshDataSuccess(List<CompanyTest> entities) {
+        if (entities != null && entities.size() > 0) {
+            adapter.bind(entities);
+        }
+        mSwipeRefreshWidget.setRefreshing(false);
+        loading_layout.setLoadingLayout(LoadingLayout.HIDE_LAYOUT);
+    }
+
+    @Override
+    public void loadMoreWeekDataSuccess(final List<CompanyTest> entities) {
+        int delay = 0;
+        if (TimeUtils.getCurrentTime() - currentTime < DEF_DELAY) {
+            delay = DEF_DELAY;
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                currentState = STATE_NORMAL;
+                if (entities.isEmpty()) {
+                    adapter.setHasMoreDataAndFooter(false, false);
+                    FrameManager.getInstance().toastPrompt("没有更多数据...");
+                } else {
+                    adapter.appendToList(entities);
+                    adapter.setHasMoreDataAndFooter(true, false);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }, delay);
     }
 
 }
