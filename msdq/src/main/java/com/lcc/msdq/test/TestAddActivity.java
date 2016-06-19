c2bdb7315412f0d43b31fbcccf0679aeb99ba34b
liangchengcheng <1038127753@qq.com>
2016-06-19 16:34:08 +0800

    添加菜单

---
+++ msdq/src/main/java/com/lcc/msdq/test/TestAddActivity.java
@@ -0,0 +1,27 @@
+package com.lcc.msdq.test;
+
+import com.lcc.base.BaseActivity;
+
+/**
+ * Author:       梁铖城
+ * Email:        1038127753@qq.com
+ * Date:         2015年11月21日15:28:25
+ * Description:  TestAddActivity
+ */
+public class TestAddActivity extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected boolean Open() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutView() {
+        return 0;
+    }
+}
