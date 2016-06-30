38838239783c8e4490e768448e340dcf96c706d2
liangchengcheng <1038127753@qq.com>
2016-06-30 13:07:22 +0800

    动态的欢迎的界面

--- msdq/src/main/java/com/lcc/msdq/SplashActivity.java
+++ msdq/src/main/java/com/lcc/msdq/SplashActivity.java
@@ -8,15 +8,36 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
 import com.lcc.msdq.choice.ChoiceTypeoneActivity;
 import com.lcc.utils.SharePreferenceUtil;
 
-public class SplashActivity extends Activity {
+import java.util.Calendar;
 
+import zsbpj.lccpj.frame.ImageManager;
+import zsbpj.lccpj.utils.LogUtils;
+
+public class SplashActivity extends Activity {
+    private ImageView ly;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ly= (ImageView) findViewById(R.id.ly);
+        Calendar calendar= Calendar.getInstance();
+        int hour=calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour>6&&hour<12){
+            ImageManager.getInstance().loadResImage(SplashActivity.this,
+                    R.drawable.morning,ly);
+        }else if (hour>12&&hour<18){
+            ImageManager.getInstance().loadResImage(SplashActivity.this,
+                    R.drawable.afternoon,ly);
+        }else {
+            ImageManager.getInstance().loadResImage(SplashActivity.this,
+                    R.drawable.night,ly);
+        }
+
         FrameLayout reveal = (FrameLayout) findViewById(R.id.reveal);
         getWelcomeView(reveal);
     }
