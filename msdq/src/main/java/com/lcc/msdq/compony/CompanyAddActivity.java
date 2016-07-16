610ae07c3d05e3eca1887706f0338efb4e5b22a5
liangchengcheng <1038127753@qq.com>
2016-07-16 21:55:52 +0800

    地址的选择，后期会修改今天先写到这里

--- msdq/src/main/java/com/lcc/msdq/compony/CompanyAddActivity.java
+++ msdq/src/main/java/com/lcc/msdq/compony/CompanyAddActivity.java
@@ -2,10 +2,14 @@ package com.lcc.msdq.compony;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.TextView;
 
 import com.lcc.base.BaseActivity;
 import com.lcc.entity.CompanyDescription;
 import com.lcc.msdq.R;
+
+import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Author:       梁铖城
@@ -17,16 +21,29 @@ public class CompanyAddActivity extends BaseActivity {
 
     public static final String NAME = "name";
     private String name;
+    private Pattern intPattern = Pattern.compile("^[-\\+]?[\\d]*\\.0*$");
+    private String pro, city, dis;
 
-    public static void startCompanyAddActivity(String cn, Activity startingActivity) {
-        Intent intent = new Intent(startingActivity, CompanyAddActivity.class);
-        intent.putExtra(NAME, cn);
-        startingActivity.startActivity(intent);
+    private TextView tv_position;
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.com_des_add;
     }
 
     @Override
     protected void initView() {
+        tv_position= (TextView) findViewById(R.id.tv_position);
+        initData();
+    }
 
+    private void initData() {
+        final Map map = (Map) getIntent().getSerializableExtra("addressInfo");
+        pro = getString(map, "provName", "");
+        city = getString(map, "cityName", "");
+        dis = getString(map, "districtName", "");
+        String areaName = String.format("%s %s %s", pro, city, dis);
+        tv_position.setText(areaName);
     }
 
     @Override
@@ -34,8 +51,11 @@ public class CompanyAddActivity extends BaseActivity {
         return false;
     }
 
-    @Override
-    protected int getLayoutView() {
-        return R.layout.com_des_add;
+
+    public String getString(Map map, String key, String defaultValue) {
+        Object obj = map.get(key);
+        return obj == null ? defaultValue : (obj instanceof Number &&
+                intPattern.matcher(obj.toString()).matches() ?
+                String.valueOf(Long.valueOf(((Number) obj).longValue())) : obj.toString());
     }
 }
