ac9b854a383595f834102e83f11cdf28dd9b7827
liangchengcheng <1038127753@qq.com>
2016-07-19 13:42:26 +0800

    消息界面增加基本的布局

--- msdq/src/main/java/com/lcc/msdq/news/znews/ZNewsActivity.java
+++ msdq/src/main/java/com/lcc/msdq/news/znews/ZNewsActivity.java
@@ -7,7 +7,7 @@ import com.lcc.msdq.R;
  * Author:       梁铖城
  * Email:        1038127753@qq.com
  * Date:         2015年11月21日15:28:25
- * Description:  XtNewsActivity
+ * Description:  ZNewsActivity
  */
 public class ZNewsActivity extends BaseActivity {
 
