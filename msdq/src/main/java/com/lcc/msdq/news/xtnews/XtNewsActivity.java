7b726155dcaad45f399122aab9240e1d9556b8be
liangchengcheng <1038127753@qq.com>
2016-07-19 16:28:03 +0800

    未读消息的mvp

--- msdq/src/main/java/com/lcc/msdq/news/xtnews/XtNewsActivity.java
+++ msdq/src/main/java/com/lcc/msdq/news/xtnews/XtNewsActivity.java
@@ -1,7 +1,19 @@
 package com.lcc.msdq.news.xtnews;
 
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.lcc.adapter.IndexMenuAdapter;
+import com.lcc.adapter.XtNewsAdapter;
 import com.lcc.base.BaseActivity;
+import com.lcc.entity.XtNewsEntity;
 import com.lcc.msdq.R;
+import com.lcc.view.loadview.LoadingLayout;
+
+import zsbpj.lccpj.utils.TimeUtils;
+import zsbpj.lccpj.view.recyclerview.listener.OnRecycleViewScrollListener;
 
 /**
  * Author:       梁铖城
@@ -9,11 +21,16 @@ import com.lcc.msdq.R;
  * Date:         2015年11月21日15:28:25
  * Description:  XtNewsActivity
  */
-public class XtNewsActivity extends BaseActivity {
+public class XtNewsActivity extends BaseActivity implements XtNewsAdapter.OnItemClickListener {
+
+    private LoadingLayout loading_layout;
+    private RecyclerView mRecyclerView;
+    private XtNewsAdapter mAdapter;
 
     @Override
     protected void initView() {
-
+        initRecycleView();
+        loading_layout = (LoadingLayout) findViewById(R.id.loading_layout);
     }
 
     @Override
@@ -24,5 +41,21 @@ public class XtNewsActivity extends BaseActivity {
     @Override
     protected int getLayoutView() {
         return R.layout.xt_news_layout;
+    }
+
+    @Override
+    public void onItemClick(XtNewsEntity data) {
+
+    }
+
+    private void initRecycleView() {
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(XtNewsActivity.this,
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter = new XtNewsAdapter();
+        mAdapter.setOnItemClickListener(this);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ msdq/src/main/java/com/lcc/mvp/model/XtNewsModel.java
@@ -0,0 +1,22 @@
+package com.lcc.mvp.model;
+
+
+import com.lcc.AppConstants;
+import com.lcc.api.ApiClient;
+import com.lcc.api.ParamsMap;
+import com.lcc.frame.net.okhttp.callback.ResultCallback;
+import com.lcc.frame.net.okhttp.request.OkHttpRequest;
+
+public class XtNewsModel {
+
+    /**
+     * 获取未读的系统消息
+     */
+    public OkHttpRequest getXtNews( ResultCallback<String> callback) {
+        ParamsMap paramsMap = new ParamsMap();
+        paramsMap.put(AppConstants.ParamKey.AUTHOR, "18813149871");
+        return ApiClient.create(AppConstants.RequestPath.ARTICLE_DATA, paramsMap).tag("").get(callback);
+    }
+
+
+}
