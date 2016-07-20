5d4e0f0c6f0728f74c7d415e6b5f4c2dfcd0c74c
liangchengcheng <1038127753@qq.com>
2016-07-20 22:20:23 +0800

    个人资料

--- msdq/src/main/java/com/lcc/msdq/description/other/OtherUserProfileActivity.java
+++ msdq/src/main/java/com/lcc/msdq/description/other/OtherUserProfileActivity.java
@@ -1,4 +1,4 @@
-package com.lcc.msdq.description.user;
+package com.lcc.msdq.description.other;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -8,8 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
@@ -22,9 +20,8 @@ import android.widget.TextView;
 import com.lcc.base.BaseActivity;
 import com.lcc.db.test.UserInfo;
 import com.lcc.msdq.R;
-import com.lcc.msdq.compony.content.CodeFragment;
-import com.lcc.msdq.compony.content.HrFragment;
-import com.lcc.msdq.compony.content.OtherFragment;
+import com.lcc.msdq.description.user.UserEditActivity;
+import com.lcc.msdq.description.user.UserProfileAdapter;
 import com.lcc.msdq.favorite.ArticleFragment;
 import com.lcc.msdq.flow.FlowIndex;
 
@@ -35,7 +32,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import zsbpj.lccpj.frame.ImageManager;
 
-public class UserProfileActivity extends BaseActivity implements View.OnClickListener{
+public class OtherUserProfileActivity extends BaseActivity implements View.OnClickListener{
 
     public static final String ARG_REVEAL_START_LOCATION = "reveal_start_location";
     private static final int USER_OPTIONS_ANIMATION_DELAY = 300;
@@ -47,6 +44,8 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
     ImageView ivUserProfilePhoto;
     @Bind(R.id.vUserDetails)
     View vUserDetails;
+    @Bind(R.id.btnFollow)
+    Button btnFollow;
     @Bind(R.id.vUserStats)
     View vUserStats;
     @Bind(R.id.vUserProfileRoot)
@@ -59,8 +58,6 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
     LinearLayout tv_me;
     @Bind(R.id.tv_you)
     LinearLayout tv_you;
-    @Bind(R.id.iv_edit)
-    ImageView iv_edit;
 
     private int avatarSize;
     private String profilePhoto;
@@ -69,7 +66,7 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
 
     public static void startUserProfileFromLocation(int[] startingLocation,
                                                     Activity startingActivity) {
-        Intent intent = new Intent(startingActivity, UserProfileActivity.class);
+        Intent intent = new Intent(startingActivity, OtherUserProfileActivity.class);
         intent.putExtra(ARG_REVEAL_START_LOCATION, startingLocation);
         startingActivity.startActivity(intent);
     }
@@ -83,8 +80,7 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
 
         tv_me.setOnClickListener(this);
         tv_you.setOnClickListener(this);
-        iv_edit.setOnClickListener(this);
-        ImageManager.getInstance().loadCircleImage(UserProfileActivity.this,
+        ImageManager.getInstance().loadCircleImage(OtherUserProfileActivity.this,
                 userInfo.getUser_image(),
                 ivUserProfilePhoto);
         tv_nickname.setText(userInfo.getNickname());
@@ -108,7 +104,7 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
 
     @Override
     protected int getLayoutView() {
-        return R.layout.activity_user_profile;
+        return R.layout.activity_other_user_profile;
     }
 
     private void setViewPager() {
@@ -140,16 +136,13 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
         switch (v.getId()){
 
             case R.id.tv_me:
-                FlowIndex.startUserProfileFromLocation("me","18813149871",UserProfileActivity.this);
+                FlowIndex.startUserProfileFromLocation("me","18813149871",OtherUserProfileActivity.this);
                 break;
 
             case R.id.tv_you:
-                FlowIndex.startUserProfileFromLocation("you","18813149871",UserProfileActivity.this);
+                FlowIndex.startUserProfileFromLocation("you","18813149871",OtherUserProfileActivity.this);
                 break;
 
-            case R.id.iv_edit:
-                startActivity(new Intent(UserProfileActivity.this,UserEditActivity.class));
-                break;
         }
     }
 
