6a4020a2558ca645a5433a1249d1c4d353da522c
liangchengcheng <1038127753@qq.com>
2016-07-31 17:00:33 +0800

    细节调整之布局的调整mvp

---
+++ msdq/src/main/java/com/lcc/mvp/model/QuestionAddModel.java
@@ -0,0 +1,33 @@
+package com.lcc.mvp.model;
+
+
+import com.lcc.AppConstants;
+import com.lcc.api.ApiClient;
+import com.lcc.api.ParamsMap;
+import com.lcc.entity.CompanyDescription;
+import com.lcc.entity.QuestionAdd;
+import com.lcc.frame.net.okhttp.callback.ResultCallback;
+import com.lcc.frame.net.okhttp.request.OkHttpRequest;
+
+import java.io.File;
+import java.util.List;
+
+public class QuestionAddModel {
+
+    /**
+     * 增加发布新的问题
+     */
+    public OkHttpRequest QuestionAdd(QuestionAdd questionAdd, List<File>files, ResultCallback<String> callback) {
+        ParamsMap paramsMap = new ParamsMap();
+        paramsMap.put("type", questionAdd.getType());
+        paramsMap.put("option", questionAdd.getOptions());
+        paramsMap.put("title", questionAdd.getTitle());
+        paramsMap.put("summary", questionAdd.getSummary());
+        paramsMap.put("source", questionAdd.getSource());
+        paramsMap.put("keyword", questionAdd.getKeyword());
+
+        return ApiClient.createWithFile(AppConstants.RequestPath.saveCompanyForAndroid,
+                paramsMap,files)
+                .upload(callback);
+    }
+}
