fa3732b17c9ce76494cc296ba5ea71ae1b92c0aa
liangchengcheng <1038127753@qq.com>
2016-07-31 18:08:19 +0800

    细节调整之布局的调整基本功能

--- msdq/src/main/java/com/lcc/msdq/test/answer/AnswerAddActivity.java
+++ msdq/src/main/java/com/lcc/msdq/test/answer/AnswerAddActivity.java
@@ -1,32 +1,46 @@
 package com.lcc.msdq.test.answer;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lcc.base.BaseActivity;
+import com.lcc.msdq.PhotoPickerActivity;
 import com.lcc.msdq.R;
 import com.lcc.msdq.test.answer.photo.UILImageLoader;
 import com.lcc.msdq.test.answer.photo.UILPauseOnScrollListener;
 import com.lcc.rich.RichEditor;
 import com.lcc.utils.KeyboardUtils;
+import com.lcc.view.edit.editor.SEditorData;
+import com.lcc.view.edit.editor.SortRichEditor;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import cn.finalteam.galleryfinal.CoreConfig;
@@ -41,316 +55,50 @@ import cn.finalteam.galleryfinal.model.PhotoInfo;
  * Date:         2015年11月21日15:28:25
  * Description:  AnswerAddActivity
  */
-public class AnswerAddActivity extends BaseActivity {
+public class AnswerAddActivity extends BaseActivity implements View.OnClickListener {
 
-    private RichEditor mEditor;
-    private TextView mPreview;
-    private final int REQUEST_CODE_CAMERA = 1000;
-    private final int REQUEST_CODE_GALLERY = 1001;
+    public static final int REQUEST_CODE_PICK_IMAGE = 1023;
+    public static final int REQUEST_CODE_CAPTURE_CAMEIA = 1022;
 
-    public FunctionConfig functionConfig;
+    private static final File PHOTO_DIR = new File(
+            Environment.getExternalStorageDirectory() + "/DCIM/Camera");
+
+    private TextView tvSort;
+
+    private SortRichEditor editor;
+
+    private ImageView ivGallery, ivCamera;
+
+    private Button btnPosts;
+    // 照相机拍照得到的图片
+    private File mCurrentPhotoFile;
+
+    public Intent getTakePickIntent(File f) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE, null);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tvSort = (TextView) findViewById(R.id.tv_sort);
+        editor = (SortRichEditor) findViewById(R.id.richEditor);
+        ivGallery = (ImageView) findViewById(R.id.iv_gallery);
+        ivCamera = (ImageView) findViewById(R.id.iv_camera);
+        btnPosts = (Button) findViewById(R.id.btn_posts);
+
+        tvSort.setOnClickListener(this);
+        ivGallery.setOnClickListener(this);
+        ivCamera.setOnClickListener(this);
+        btnPosts.setOnClickListener(this);
+    }
 
     @Override
     protected void initView() {
 
-        ThemeConfig themeConfig = ThemeConfig.GREEN;
-        //配置功能
-        functionConfig = new FunctionConfig.Builder()
-                .setEnableCamera(true)
-                .setEnableEdit(true)
-                .setEnableCrop(true)
-                .setEnableRotate(true)
-                .setCropSquare(true)
-                .setEnablePreview(true)
-                .setMutiSelectMaxSize(8)
-                .build();
-        CoreConfig coreConfig = new CoreConfig.Builder(this, new UILImageLoader(), themeConfig)
-                .setFunctionConfig(functionConfig)
-                .setPauseOnScrollListener(new UILPauseOnScrollListener(false, true))
-                .build();
-        GalleryFinal.init(coreConfig);
-
-
-        mEditor = (RichEditor) findViewById(R.id.editor);
-        mEditor.setEditorHeight(200);
-        mEditor.setEditorFontSize(22);
-        mEditor.setEditorFontColor(Color.BLACK);
-        //mEditor.setEditorBackgroundColor(Color.BLUE);
-        //mEditor.setBackgroundColor(Color.BLUE);
-        //mEditor.setBackgroundResource(R.drawable.bg);
-        mEditor.setPadding(10, 10, 10, 10);
-        //    mEditor.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
-        mEditor.setPlaceholder("请在此输入你的答案...");
-
-        mPreview = (TextView) findViewById(R.id.preview);
-        mEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
-            @Override
-            public void onTextChange(String text) {
-                mPreview.setText(text);
-            }
-        });
-
-        findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.undo();
-            }
-        });
-
-        findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.redo();
-            }
-        });
-
-        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setBold();
-            }
-        });
-
-        findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setItalic();
-            }
-        });
-
-        findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setSubscript();
-            }
-        });
-
-        findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setSuperscript();
-            }
-        });
-
-        findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setStrikeThrough();
-            }
-        });
-
-        findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setUnderline();
-            }
-        });
-
-        findViewById(R.id.action_heading1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(1);
-            }
-        });
-
-        findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(2);
-            }
-        });
-
-        findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(3);
-            }
-        });
-
-        findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(4);
-            }
-        });
-
-        findViewById(R.id.action_heading5).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(5);
-            }
-        });
-
-        findViewById(R.id.action_heading6).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setHeading(6);
-            }
-        });
-
-        findViewById(R.id.action_txt_color).setOnClickListener(new View.OnClickListener() {
-            private boolean isChanged;
-
-            @Override
-            public void onClick(View v) {
-                mEditor.setTextColor(isChanged ? Color.BLACK : Color.RED);
-                isChanged = !isChanged;
-            }
-        });
-
-        findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
-            private boolean isChanged;
-
-            @Override
-            public void onClick(View v) {
-                mEditor.setTextBackgroundColor(isChanged ? Color.WHITE : Color.GRAY);
-                isChanged = !isChanged;
-            }
-        });
-
-        findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setIndent();
-            }
-        });
-
-        findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setOutdent();
-            }
-        });
-
-        findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setAlignLeft();
-            }
-        });
-
-        findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setAlignCenter();
-            }
-        });
-
-        findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setAlignRight();
-            }
-        });
-
-        findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.setBlockquote();
-            }
-        });
-
-        findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                KeyboardUtils.hide(AnswerAddActivity.this);
-                showPopMenu();
-            }
-        });
-
-        findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.insertLink("https://github.com/wasabeef", "wasabeef");
-            }
-        });
-        findViewById(R.id.action_insert_checkbox).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditor.insertTodo();
-            }
-        });
     }
-
-    private void showPopMenu() {
-        initImageLoader(AnswerAddActivity.this);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.dialog_layout, null);
-        //下面是两种方法得到宽度和高度 getWindow().getDecorView().getWidth()
-        final PopupWindow window = new PopupWindow(view,
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.WRAP_CONTENT);
-        window.setFocusable(true);
-        ColorDrawable dw = new ColorDrawable(0xb0000000);
-        window.setBackgroundDrawable(dw);
-        window.setAnimationStyle(R.style.mypopwindow_anim_style);
-        window.showAtLocation(AnswerAddActivity.this.findViewById(R.id.rootview),
-                Gravity.BOTTOM, 0, 0);
-        window.setOnDismissListener(new PopupWindow.OnDismissListener() {
-
-            @Override
-            public void onDismiss() {
-            }
-        });
-        view.findViewById(R.id.ll_qx).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                window.dismiss();
-            }
-        });
-        view.findViewById(R.id.ll_xc).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GalleryFinal.openGalleryMuti(REQUEST_CODE_GALLERY, functionConfig, mOnHanlderResultCallback);
-                window.dismiss();
-            }
-        });
-        view.findViewById(R.id.ll_photo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GalleryFinal.openCamera(REQUEST_CODE_CAMERA, functionConfig, mOnHanlderResultCallback);
-                window.dismiss();
-            }
-        });
-    }
-
-    private void initImageLoader(Context context) {
-        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
-        config.threadPriority(Thread.NORM_PRIORITY - 2);
-        config.denyCacheImageMultipleSizesInMemory();
-        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
-        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
-        config.tasksProcessingOrder(QueueProcessingType.LIFO);
-        config.writeDebugLogs();
-        ImageLoader.getInstance().init(config.build());
-    }
-
-    private GalleryFinal.OnHanlderResultCallback mOnHanlderResultCallback = new GalleryFinal.OnHanlderResultCallback() {
-        @Override
-        public void onHanlderSuccess(int reqeustCode, List<PhotoInfo> resultList) {
-            if (resultList != null) {
-                for (int i = 0; i < resultList.size(); i++) {
-                    Log.e("lcc", resultList.get(i).getPhotoPath());
-                    // TODO: 16/5/31  /storage/emulated/0/======= file://mnt//sdcard
-                    String extra_Path = Environment.getExternalStorageDirectory().getPath();
-                    Log.e("lcc", extra_Path);
-                    Bitmap b = BitmapFactory.decodeFile(resultList.get(0).getPhotoPath());
-                    try {
-                        Thread.sleep(200);
-                        mEditor.insertImage("file:"+resultList.get(i).getPhotoPath(),
-                                "dachshund");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void onHanlderFailure(int requestCode, String errorMsg) {
-            Toast.makeText(AnswerAddActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
-        }
-    };
 
     @Override
     protected boolean Open() {
@@ -359,6 +107,80 @@ public class AnswerAddActivity extends BaseActivity {
 
     @Override
     protected int getLayoutView() {
-        return R.layout.answer_add;
+        return R.layout.answer_add_activity;
     }
+
+    /**
+     * 负责处理编辑数据提交等事宜，请自行实现
+     */
+    private void dealEditData(List<SEditorData> editList) {
+        for (SEditorData itemData : editList) {
+            if (itemData.getInputStr() != null) {
+                Log.e("RichEditor", "commit inputStr=" + itemData.getInputStr());
+            } else if (itemData.getImagePath() != null) {
+                Log.e("RichEditor", "commit imgePath=" + itemData.getImagePath());
+            }
+        }
+    }
+
+    private void openCamera() {
+        try {
+            // 创建照片的存储目录
+            PHOTO_DIR.mkdirs();
+            // 给新照的照片文件命名
+            mCurrentPhotoFile = new File(PHOTO_DIR, getPhotoFileName());
+            final Intent intent = getTakePickIntent(mCurrentPhotoFile);
+            startActivityForResult(intent, REQUEST_CODE_CAPTURE_CAMEIA);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 用当前时间给取得的图片命名
+     */
+    private String getPhotoFileName() {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "'IMG'_yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(date) + ".jpg";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+
+        if (editor.isSort()) tvSort.setText("排序");
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            String[] photoPaths = data.getStringArrayExtra(PhotoPickerActivity.INTENT_PHOTO_PATHS);
+            editor.addImageArray(photoPaths);
+        } else if (requestCode == REQUEST_CODE_CAPTURE_CAMEIA) {
+            editor.addImage(mCurrentPhotoFile.getAbsolutePath());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_sort:
+                if (editor.sort()) {
+                    tvSort.setText("完成");
+                } else {
+                    tvSort.setText("排序");
+                }
+                break;
+            case R.id.iv_gallery:
+                startActivityForResult(new Intent(this, PhotoPickerActivity.class), REQUEST_CODE_PICK_IMAGE);
+                break;
+            case R.id.iv_camera:
+                openCamera();
+                break;
+            case R.id.btn_posts:
+                List<SEditorData> editList = editor.buildEditData();
+                // 下面的代码可以上传、或者保存，请自行实现
+                dealEditData(editList);
+                break;
+        }
+    }
+
 }
