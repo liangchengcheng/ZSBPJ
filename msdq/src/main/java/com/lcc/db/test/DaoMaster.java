48cb163ee0b4fdab65e71efd3f052636734da660
liangchengcheng <1038127753@qq.com>
2016-08-12 22:43:14 +0800

    细节调整之调整公司介绍

--- msdq/src/main/java/com/lcc/db/test/DaoMaster.java
+++ msdq/src/main/java/com/lcc/db/test/DaoMaster.java
@@ -12,7 +12,7 @@ import de.greenrobot.dao.AbstractDaoMaster;
 import de.greenrobot.dao.identityscope.IdentityScopeType;
 
 public class DaoMaster extends AbstractDaoMaster {
-    public static final int SCHEMA_VERSION = 2;
+    public static final int SCHEMA_VERSION = 1;
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(SQLiteDatabase db, boolean ifNotExists) {
