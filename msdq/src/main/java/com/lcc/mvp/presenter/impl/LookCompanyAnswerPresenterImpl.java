66ea9aefeedae15734ca48c20adf0533655f49a9
liangchengcheng <1038127753@qq.com>
2016-08-27 17:35:48 +0800

    mvp

--- msdq/src/main/java/com/lcc/mvp/presenter/impl/LookCompanyAnswerPresenterImpl.java
+++ msdq/src/main/java/com/lcc/mvp/presenter/impl/LookCompanyAnswerPresenterImpl.java
@@ -3,18 +3,13 @@ package com.lcc.mvp.presenter.impl;
 import android.os.Handler;
 import android.text.TextUtils;
 
-import com.lcc.entity.Answer;
-import com.lcc.entity.Article;
 import com.lcc.entity.CompanyAnswer;
 import com.lcc.entity.CompanyTest;
 import com.lcc.entity.FavEntity;
 import com.lcc.frame.net.okhttp.callback.ResultCallback;
 import com.lcc.mvp.model.CompanyAnswerModel;
-import com.lcc.mvp.model.TestAnswerModel;
 import com.lcc.mvp.presenter.CompanyAnswerPresenter;
-import com.lcc.mvp.presenter.TestAnswerPresenter;
 import com.lcc.mvp.view.CompanyAnswerView;
-import com.lcc.mvp.view.TestAnswerView;
 import com.squareup.okhttp.Request;
 
 import org.json.JSONObject;
@@ -25,13 +20,12 @@ import zsbpj.lccpj.frame.ApiException;
 import zsbpj.lccpj.utils.GsonUtils;
 import zsbpj.lccpj.utils.TimeUtils;
 
-public class CompanyAnswerPresenterImpl implements CompanyAnswerPresenter {
-
+public class LookCompanyAnswerPresenterImpl implements CompanyAnswerPresenter {
     private static final int DEF_DELAY = (int) (1 * 1000);
     private CompanyAnswerModel model;
     private CompanyAnswerView view;
 
-    public CompanyAnswerPresenterImpl(CompanyAnswerView view) {
+    public LookCompanyAnswerPresenterImpl(CompanyAnswerView view) {
         this.view = view;
         model = new CompanyAnswerModel();
     }
