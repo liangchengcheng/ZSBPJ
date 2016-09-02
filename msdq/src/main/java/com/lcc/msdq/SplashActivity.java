7cd0be31faee6e406a598904a461412e89b1289b
liangchengcheng <1038127753@qq.com>
2016-09-02 22:07:24 +0800

    开始优化

--- msdq/src/main/java/com/lcc/msdq/SplashActivity.java
+++ msdq/src/main/java/com/lcc/msdq/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.lcc.msdq;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,9 +8,13 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.lcc.App;
 import com.lcc.msdq.login.LoginActivity;
 import com.lcc.msdq.login.SignUpActivity;
@@ -18,9 +23,11 @@ import java.util.Calendar;
 import zsbpj.lccpj.frame.ImageManager;
 
 public class SplashActivity extends Activity implements View.OnClickListener {
-
     private LinearLayout ll_bottom_view;
     private String user_tk;
+    private ImageView logo_inner_iv;
+    boolean isShowingRubberEffect = false;
+    private TextView app_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,8 @@ public class SplashActivity extends Activity implements View.OnClickListener {
         App.addActivity(this);
         user_tk = SharePreferenceUtil.getUserTk();
 
+        app_name = (TextView) findViewById(R.id.app_name);
+        logo_inner_iv = (ImageView) findViewById(R.id.logo_inner_iv);
         ImageView ly = (ImageView) findViewById(R.id.ly);
         ll_bottom_view = (LinearLayout) findViewById(R.id.ll_bottom_view);
         findViewById(R.id.tv_toregister).setOnClickListener(this);
@@ -40,7 +49,9 @@ public class SplashActivity extends Activity implements View.OnClickListener {
         } else {
             ll_bottom_view.setVisibility(View.GONE);
         }
-        getWelcomeView(reveal);
+        initAnimation();
+        //渐隐模式
+        //getWelcomeView(reveal);
     }
 
     private void changePic(ImageView ly){
@@ -104,6 +115,45 @@ public class SplashActivity extends Activity implements View.OnClickListener {
                 finish();
                 break;
         }
+    }
+
+    private void initAnimation() {
+        startLogoInner1();
+        startLogoOuterAndAppName();
+    }
+
+    private void startLogoInner1() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_top_in);
+        logo_inner_iv.startAnimation(animation);
+    }
+
+    private void startLogoOuterAndAppName() {
+        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(1000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = animation.getAnimatedFraction();
+                if (fraction >= 0.8 && !isShowingRubberEffect) {
+                    isShowingRubberEffect = true;
+                    startShowAppName();
+                    JumpNextPage();
+                } else if (fraction >= 0.95) {
+                    valueAnimator.cancel();
+                    startLogoInner2();
+                }
+
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private void startShowAppName() {
+        YoYo.with(Techniques.FadeIn).duration(1000).playOn(app_name);
+    }
+
+    private void startLogoInner2() {
+        YoYo.with(Techniques.RubberBand).duration(1000).playOn(logo_inner_iv);
     }
 
 }
