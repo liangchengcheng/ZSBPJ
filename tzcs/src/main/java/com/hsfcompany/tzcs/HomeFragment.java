bc46d2a5db1880c3c4e40c6aa3f13ab68c6a17cc
liangchengcheng <1038127753@qq.com>
2017-05-13 15:05:27 +0800

    首页相关

--- tzcs/src/main/java/com/hsfcompany/tzcs/HomeFragment.java
+++ tzcs/src/main/java/com/hsfcompany/tzcs/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hsfcompany.tzcs.base.BaseFragment;
+import com.hsfcompany.tzcs.view.ColorArcProgressBar;
 
 /**
  * Author:       |梁铖城
@@ -14,9 +15,13 @@ import com.hsfcompany.tzcs.base.BaseFragment;
  * Description:  |首页的布局
  */
 public class HomeFragment extends BaseFragment {
+    private ColorArcProgressBar bar2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_fragment,null);return view;
+        View view = inflater.inflate(R.layout.home_fragment,null);
+        bar2 = (ColorArcProgressBar) view.findViewById(R.id.bar2);
+        bar2.setCurrentValues(18);
+        return view;
     }
 }
