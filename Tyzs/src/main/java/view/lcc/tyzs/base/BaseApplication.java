e32aec31bf87138ab972e6914600b67d7a4f0eee
liangchengcheng <1038127753@qq.com>
2017-08-02 09:03:39 +0800

    new  project

--- Tyzs/src/main/java/view/lcc/tyzs/base/BaseApplication.java
+++ Tyzs/src/main/java/view/lcc/tyzs/base/BaseApplication.java
@@ -2,13 +2,35 @@ package view.lcc.tyzs.base;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+
 import com.squareup.okhttp.OkHttpClient;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import view.lcc.tyzs.bean.DaoMaster;
+import view.lcc.tyzs.bean.DaoSession;
 import view.lcc.tyzs.frame.Frame;
 import view.lcc.tyzs.frame.okhttp.OkHttpClientManager;
+
+import static android.database.sqlite.SQLiteDatabase.OPEN_READWRITE;
 
 //MultiDexApplication(应该使用这个)
 public class BaseApplication extends Application {
@@ -20,9 +42,74 @@ public class BaseApplication extends Application {
 
     private OkHttpClient okHttpClient;
 
-    /**
-     * 添加Activity到容器中
-     */
+    private final String db_name = "jxyh.db";
+    private final String db_path = "databases";
+    private final String DB_UPDATE = "updateDB.xml";
+    private final String E_DB = "db";
+    private static final String E_VER = "version";
+    private static final String E_SQL_EXEC = "sqlExec";
+    private static final String ATTR_VER_CODE = "ver-code";
+    private static final String ATTR_SQL = "sql";
+    private static DaoSession daoSession;
+    private SQLiteDatabase db;
+    private int mDbVersion;
+
+    private static BaseApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Frame.setAppContext(this);
+        Frame.getInstance().init();
+        initOkHttp();
+        mInstance = this;
+
+        //数据库升级
+        new Thread() {
+
+            @Override
+            public void run() {
+                super.run();
+                File dir = new File(getFilesDir().getParent(), db_path);
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                File dbFile = new File(dir, db_name);
+
+                if (!dbFile.exists()) {
+                    try {
+                        dbFile.createNewFile();
+                        FileOutputStream fos = new FileOutputStream(dbFile);
+                        InputStream inputStream = getAssets().open(db_name);
+
+                        byte[] buffer = new byte[10240];
+                        int len;
+                        while ((len = inputStream.read(buffer)) != -1) {
+                            fos.write(buffer, 0, len);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    db = SQLiteDatabase.openDatabase(dbFile.getPath(), null, OPEN_READWRITE);
+                    updateDB();
+                }
+
+                db = SQLiteDatabase.openDatabase(dbFile.getPath(), null, OPEN_READWRITE);
+                DaoMaster daoMaster = new DaoMaster(db);
+                daoSession = daoMaster.newSession();
+
+            }
+        }.start();
+
+    }
+
+    private void initOkHttp() {
+        okHttpClient = OkHttpClientManager.getInstance().getOkHttpClient();
+        okHttpClient.setConnectTimeout(CONNECT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        okHttpClient.setReadTimeout(READ_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+    }
+
     public static void addActivity(Activity activity) {
         activityList.add(activity);
     }
@@ -37,19 +124,129 @@ public class BaseApplication extends Application {
         //System.exit(0);
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Frame.setAppContext(this);
-        Frame.getInstance().init();
-        initOkHttp();
 
+    private void updateDB() {
+
+        getDbVersion();
+        InputStream inputStream = null;
+        try {
+            inputStream = getAssets().open(DB_UPDATE);
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(inputStream);
+
+            NodeList items = doc.getElementsByTagName(E_DB);
+            if (items == null || items.getLength() <= 0) {
+                return;
+            }
+            Node root = items.item(0);
+            parseDbUpdate(root);
+        } catch (Exception e) {
+        } finally {
+            if (inputStream != null)
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
     }
 
-    private void initOkHttp() {
-        okHttpClient = OkHttpClientManager.getInstance().getOkHttpClient();
-        okHttpClient.setConnectTimeout(CONNECT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-        okHttpClient.setReadTimeout(READ_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+    private void parseDbUpdate(Node root) throws Exception {
+        if (!isValidElement(root))
+            return;
+
+        NodeList items = root.getChildNodes();
+
+        boolean isNeedUpdate = false;
+        int size = items.getLength();
+        for (int i = 0; i < size; i++) {
+            Node nodeVersion = items.item(i);
+            String elemName = nodeVersion.getNodeName();
+            if (!E_VER.equalsIgnoreCase(elemName) || !isValidElement(nodeVersion))
+                continue;
+
+            Element e = (Element) nodeVersion;
+            String versionCode = e.getAttribute(ATTR_VER_CODE);
+            if (isNeedUpdate || isNeedUpdate(versionCode)) {
+                isNeedUpdate = true;
+
+                try {
+                    db.beginTransaction();
+                    parseVersionElement(e);
+                    db.execSQL(String.format("UPDATE DBINFO SET version = '%s'", versionCode));
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+            }
+        }
+    }
+
+    private boolean isNeedUpdate(String curVersion) {
+        String curNum = curVersion.replace(".", "");
+        return Integer.parseInt(curNum.trim()) > mDbVersion;
+    }
+
+    private void getDbVersion() {
+        String dbNum = getCurDBVersion().replace(".", "");
+        mDbVersion = Integer.parseInt(dbNum.trim());
+    }
+
+    private String getCurDBVersion() {
+        String version = null;
+        Cursor c = null;
+        try {
+            c = db.rawQuery("select version from DBINFO", null);
+            if (c != null && c.moveToFirst())
+                version = c.getString(0);
+
+            return version;
+        } finally {
+            if (c != null)
+                c.close();
+        }
+    }
+
+    private void parseVersionElement(Element elemVer) throws Exception {
+        if (null == elemVer)
+            return;
+
+        NodeList items = elemVer.getChildNodes();
+        int count = items.getLength();
+        for (int i = 0; i < count; i++) {
+            Node item = items.item(i);
+            if (!isValidElement(item)) {
+                continue;
+            }
+
+            Element e = (Element) item;
+            String elemName = item.getNodeName();
+            if (E_SQL_EXEC.equalsIgnoreCase(elemName)) {
+                parseExecSql(e);
+            }
+        }
+    }
+
+    private void parseExecSql(Element elemExecSql) throws Exception {
+        if (null == elemExecSql)
+            return;
+
+        String sql = elemExecSql.getAttribute(ATTR_SQL);
+        db.execSQL(sql);
+    }
+
+    public static boolean isValidElement(Node item) {
+        return item != null && !TextUtils.isEmpty(item.getNodeName()) && item instanceof Element;
+    }
+
+    public static DaoSession getDaoSession() {
+        return daoSession;
+    }
+
+    public static Context getContext() {
+        return mInstance.getApplicationContext();
     }
 
 }
--- Tyzs/src/main/java/view/lcc/tyzs/bean/ShoppingCarBean.java
+++ Tyzs/src/main/java/view/lcc/tyzs/bean/ShoppingCarBean.java
@@ -1,6 +1,11 @@
 package view.lcc.tyzs.bean;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Property;
+
 import java.io.Serializable;
+import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * Author:       |梁铖城
@@ -8,9 +13,13 @@ import java.io.Serializable;
  * Date:         |08-01 08:57
  * Description:  |
  */
+@Entity(nameInDb = "ShoppingCarBean", createInDb = false)
 public class ShoppingCarBean implements Serializable {
+    static final long serialVersionUID = 1L;
 
     //商品id
+    @Id
+    @Property(nameInDb = "GID")
     private String GID;
 
     //零售价
@@ -36,4 +45,95 @@ public class ShoppingCarBean implements Serializable {
 
     //商品数量
     private String number;
+
+    @Generated(hash = 524153276)
+    public ShoppingCarBean(String GID, String price, String cost, String profit,
+            String name, String description, String ImageUrl, String IsDelete,
+            String number) {
+        this.GID = GID;
+        this.price = price;
+        this.cost = cost;
+        this.profit = profit;
+        this.name = name;
+        this.description = description;
+        this.ImageUrl = ImageUrl;
+        this.IsDelete = IsDelete;
+        this.number = number;
+    }
+
+    @Generated(hash = 281679540)
+    public ShoppingCarBean() {
+    }
+
+    public String getGID() {
+        return this.GID;
+    }
+
+    public void setGID(String GID) {
+        this.GID = GID;
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCost() {
+        return this.cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getProfit() {
+        return this.profit;
+    }
+
+    public void setProfit(String profit) {
+        this.profit = profit;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return this.ImageUrl;
+    }
+
+    public void setImageUrl(String ImageUrl) {
+        this.ImageUrl = ImageUrl;
+    }
+
+    public String getIsDelete() {
+        return this.IsDelete;
+    }
+
+    public void setIsDelete(String IsDelete) {
+        this.IsDelete = IsDelete;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
