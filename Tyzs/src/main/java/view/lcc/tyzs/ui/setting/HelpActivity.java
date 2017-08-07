4a65c1fcb21a2ba91ee866477ac1f77a67399edf
liangchengcheng <1038127753@qq.com>
2017-08-07 21:00:23 +0800

    new  project

---
+++ Tyzs/src/main/java/view/lcc/tyzs/ui/setting/HelpActivity.java
@@ -0,0 +1,22 @@
+package view.lcc.tyzs.ui.setting;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import view.lcc.tyzs.R;
+import view.lcc.tyzs.base.BaseActivity;
+
+/**
+ * Author:       |梁铖城
+ * Email:        |1038127753@qq.com
+ * Date:         |08-07 20:51
+ * Description:  |
+ */
+public class HelpActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_activity);
+    }
+}
