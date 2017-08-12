92479e0cacac0c0525ab1ea33b6cef0afd1e78f4
liangchengcheng <1038127753@qq.com>
2017-08-12 15:33:39 +0800

    update

---
+++ Tyzs/src/main/java/view/lcc/tyzs/bean/PersonNum.java
@@ -0,0 +1,21 @@
+package view.lcc.tyzs.bean;
+
+import java.io.Serializable;
+
+/**
+ * Author:       |梁铖城
+ * Email:        |1038127753@qq.com
+ * Date:         |08-12 14:02
+ * Description:  |
+ */
+public class PersonNum implements Serializable {
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
--- Tyzs/src/main/java/view/lcc/tyzs/ui/home/PersonNumActivity.java
+++ Tyzs/src/main/java/view/lcc/tyzs/ui/home/PersonNumActivity.java
@@ -2,15 +2,19 @@ package view.lcc.tyzs.ui.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
+import android.text.TextUtils;
+import android.widget.TextView;
+import org.json.JSONObject;
 import view.lcc.tyzs.R;
 import view.lcc.tyzs.base.BaseActivity;
+import view.lcc.tyzs.bean.OrderDetails;
+import view.lcc.tyzs.bean.PersonNum;
 import view.lcc.tyzs.frame.Frame;
 import view.lcc.tyzs.mvp.presenter.PersonNumGetPresenter;
 import view.lcc.tyzs.mvp.presenter.impl.PersonNumGetPresenterImpl;
 import view.lcc.tyzs.mvp.view.PersonNumGetView;
+import view.lcc.tyzs.utils.GsonUtils;
 import view.lcc.tyzs.utils.SharePreferenceUtil;
-import view.lcc.tyzs.view.scrollnumber.MultiScrollNumber;
 
 /**
  * Author:       |梁铖城
@@ -21,6 +25,8 @@ import view.lcc.tyzs.view.scrollnumber.MultiScrollNumber;
 public class PersonNumActivity extends BaseActivity implements PersonNumGetView {
 
     private PersonNumGetPresenter personNumGetPresenter;
+    private TextView tv_num;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,40 +34,52 @@ public class PersonNumActivity extends BaseActivity implements PersonNumGetView 
         setContentView(R.layout.person_num_activity);
 
         personNumGetPresenter = new PersonNumGetPresenterImpl(this);
-
-        MultiScrollNumber scrollNumber = (MultiScrollNumber) findViewById(R.id.scroll_number);
-        scrollNumber.setTextColors(new int[]{R.color.white});
-//        scrollNumber.setTextSize(64);
-
-//        scrollNumber.setNumber(64, 2048);
-//        scrollNumber.setInterpolator(new DecelerateInterpolator());
-
-
-        scrollNumber.setScrollVelocity(1000);
-        scrollNumber.setNumber(111);
+        tv_num = (TextView) findViewById(R.id.tv_num);
+        tv_num.setText("0");
         personNumGetPresenter.PersonNumGet(SharePreferenceUtil.getName());
 
     }
 
     @Override
     public void PersonNumGetLoading() {
-        createDialog(R.string.loading);
+       createDialog(R.string.loading);
     }
 
     @Override
-    public void PersonNumGetSuccess(String msg) {
+    public void PersonNumGetSuccess(String result) {
         closeDialog();
+        try {
+            JSONObject jsonObjecte = new JSONObject(result);
+            String code = jsonObjecte.getString("resultno");
+            if (code.equals("000")) {
+                String data = result.replace("\\", "").replace("\"[", "[").replace("]\"", "]");
+                JSONObject jsonObject = new JSONObject(data);
+                String resultJson = jsonObject.getString("resultjson");
+                PersonNum personNum = GsonUtils.changeGsonToBean(resultJson, PersonNum.class);
+                if (personNum != null) {
+                    tv_num.setText(personNum.getNum());
+                } else {
+                    tv_num.setText("0");
+                }
+            }
+        } catch (Exception e) {
+            tv_num.setText("0");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void PersonNumGetFail(String msg) {
         closeDialog();
-        Frame.getInstance().toastPrompt("网络不稳定请稍后再试");
+        if (!TextUtils.isEmpty(msg)){
+            Frame.getInstance().toastPrompt(msg);
+        }else {
+            Frame.getInstance().toastPrompt("数据加载失败，请稍后再试");
+        }
     }
 
     @Override
     public void NetWorkErr(String msg) {
         closeDialog();
-        Frame.getInstance().toastPrompt("网络不稳定请稍后再试");
     }
 }
